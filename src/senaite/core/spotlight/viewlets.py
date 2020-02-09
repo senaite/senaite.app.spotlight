fe21f11d7a1f6b1cca916877f4e3fd7319379915
Ramon Bartl <rb@ridingbytes.com>
2020-02-09 22:16:23 +0100

    Fixed code headers

--- src/senaite/core/spotlight/viewlets.py
+++ src/senaite/core/spotlight/viewlets.py
@@ -2,7 +2,7 @@
 #
 # This file is part of SENAITE.CORE.SPOTLIGHT.
 #
-# SENAITE.CORE.SUPERMODEL is free software: you can redistribute it and/or
+# SENAITE.CORE.SPOTLIGHT is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 2.
 #
