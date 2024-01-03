6906de08e412b9ad90c765ba1bdc44d7d0393bad
Ramon Bartl <rb@ridingbytes.com>
2024-01-03 11:38:22 +0100

    Development is now on 2.6.0

--- src/senaite/app/spotlight/upgrades/handlers.py
+++ src/senaite/app/spotlight/upgrades/handlers.py
@@ -23,6 +23,18 @@ from senaite.app.spotlight import logger
 PROFILE_ID = "profile-senaite.app.spotlight:default"
 
 
+def to_2600(portal_setup):
+    """Update to version 2.6.0
+
+    :param portal_setup: The portal_setup tool
+    """
+    logger.info("Run all import steps from SENAITE APP SPOTLIGHT ...")
+    context = portal_setup._getImportContext(PROFILE_ID)
+    portal = context.getSite()  # noqa
+    portal_setup.runAllImportStepsFromProfile(PROFILE_ID)
+    logger.info("Run all import steps from SENAITE APP SPOTLIGHT [DONE]")
+
+
 def to_2500(portal_setup):
     """Update to version 2.5.0
 
